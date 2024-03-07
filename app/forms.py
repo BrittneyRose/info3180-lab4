0b8562d0a524992ec9e13648638713ce9b30e166
Brittney Rose <brittneyrose@Brittneys-MacBook-Air.local>
2024-03-06 20:01:54 -0500

    exercise 5

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired, DataRequired
+from flask_wtf.file import FileField, FileAllowed
 
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired()])
     password = PasswordField('Password', validators=[InputRequired()])
+
+# ex 5
+class UploadForm(FlaskForm):
+    file = FileField('Image File', validators=[DataRequired(), FileAllowed(['jpg', 'png'], 'Images Only')])
--- app/views.py
+++ app/views.py
@@ -6,7 +6,11 @@ from werkzeug.utils import secure_filename
 from app.models import UserProfile
 from app.forms import LoginForm
 from werkzeug.security import check_password_hash
+from .forms import UploadForm
 
+from app.config import Config
+
+UPLOAD_FOLDER = Config.UPLOAD_FOLDER
 ###
 # Routing for your application.
 ###
@@ -24,24 +28,27 @@ def about():
 
 
 @app.route('/upload', methods=['POST', 'GET'])
+@login_required
 def upload():
     # Instantiate your form class
+    form = UploadForm()
 
     # Validate file upload on submit
-    if form.validate_on_submit():
+    if form.validate_on_submit() and request.method == 'POST':
         # Get file data and save to your uploads folder
+        file = form.file.data
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.root_path, UPLOAD_FOLDER, filename))
 
-        flash('File Saved', 'success')
+        flash('File Saved')
         return redirect(url_for('home')) # Update this to redirect the user to a route that displays all uploaded image files
 
-    return render_template('upload.html')
+    return render_template('upload.html', form = form)
 
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     form = LoginForm()
-
-    
 
     # change this to actually validate the entire form submission
     # and not just one field
